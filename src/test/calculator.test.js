1aa20c04805f25c0ca132d67d8bfde23d78522e3
seanstory-dev <seanstory.dev@gmail.com>
2022-02-02 13:30:38 +0900

    Add calculator test

---
+++ src/test/calculator.test.js
@@ -0,0 +1,60 @@
+const Calculator = require('../calculator');
+
+describe('Calculator', () => {
+  let cal;
+  beforeEach(() => {
+    cal = new Calculator();
+  });
+
+  it('inits with 0', () => {
+    expect(cal.value).toBe(0);
+  });
+
+  it('sets', () => {
+    cal.set(9);
+    expect(cal.value).toBe(9);
+  });
+
+  it('clear', () => {
+    cal.set(9);
+    cal.clear();
+    expect(cal.value).toBe(0);
+  });
+
+  it('add', () => {
+    cal.set(9);
+    cal.add(1);
+    expect(cal.value).toBe(10);
+  });
+
+  it('substract', () => {
+    cal.set(9);
+    cal.substract(1);
+    expect(cal.value).toBe(8);
+  });
+
+  it('multiply', () => {
+    cal.set(9);
+    cal.multiply(2);
+    expect(cal.value).toBe(18);
+  });
+
+  describe('divides', () => {
+    it('0 / 0 === NaN', () => {
+      cal.divide(0);
+      expect(cal.value).toBe(NaN);
+    });
+
+    it('1 / 0 === Infinity', () => {
+      cal.set(1);
+      cal.divide(0);
+      expect(cal.value).toBe(Infinity);
+    });
+
+    it('9 / 9 === 1', () => {
+      cal.set(9);
+      cal.divide(9);
+      expect(cal.value).toBe(1);
+    });
+  });
+});

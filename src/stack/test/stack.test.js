a11868407224b8f0fadb3ce629836fec9f875071
seanstory-dev <seanstory.dev@gmail.com>
2022-02-04 11:51:10 +0900

    Add tdd practice with stack

---
+++ src/stack/test/stack.test.js
@@ -0,0 +1,48 @@
+const Stack = require('../stack');
+
+describe('Stack', () => {
+  let stack;
+
+  beforeEach(() => {
+    stack = new Stack();
+  });
+
+  it('is created empty', () => {
+    expect(stack.size()).toBe(0);
+  });
+
+  it('allows to push item', () => {
+    stack.push('💩');
+    expect(stack.size()).toBe(1);
+  });
+
+  describe('pop', () => {
+    it('throws an error if stack is empty', () => {
+      expect(() => {
+        stack.pop();
+      }).toThrow('Stack is empty.');
+    });
+
+    it('returns the last pushed item and removes it from the stack', () => {
+      stack.push('💩');
+      stack.push('🍌');
+      expect(stack.pop()).toBe('🍌');
+      expect(stack.size()).toBe(1);
+    });
+  });
+
+  describe('peek', () => {
+    it('throws an error if stack is empty', () => {
+      expect(() => {
+        stack.peek();
+      }).toThrow('Stack is empty.');
+    });
+
+    it('returns the last pushed item but keeps it from the stack', () => {
+      stack.push('💩');
+      stack.push('🍌');
+      expect(stack.peek()).toBe('🍌');
+      expect(stack.size()).toBe(2);
+    });
+  });
+});

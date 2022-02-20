125251b2999daaa4d8265130c81c8708839ca009
seanstory-dev <seanstory.dev@gmail.com>
2022-02-20 15:54:18 +0900

    Add unit test

---
+++ react-test/habit-tracker/src/habit_presenter.test.js
@@ -0,0 +1,66 @@
+import HabitPresenter from "./habit_presenter";
+
+describe("HabitPresenter", () => {
+  const habits = [
+    { id: 1, name: "Reading", count: 1 },
+    { id: 2, name: "Running", count: 0 },
+  ];
+  let presenter;
+  let update;
+
+  beforeEach(() => {
+    presenter = new HabitPresenter(habits);
+    update = jest.fn();
+  });
+
+  it("init with habits", () => {
+    expect(presenter.getHabits()).toEqual(habits);
+  });
+
+  it("increments habit count and call update callback", () => {
+    presenter.increment(habits[0], update);
+
+    expect(presenter.getHabits()[0].count).toBe(2);
+    checkUpdateIsCalled();
+  });
+
+  it("decrements habit count and call update callback", () => {
+    presenter.decrement(habits[0], update);
+
+    expect(presenter.getHabits()[0].count).toBe(0);
+    checkUpdateIsCalled();
+  });
+
+  it("does not set the count value below 0 when decrements", () => {
+    presenter.decrement(habits[0], update);
+    presenter.decrement(habits[0], update);
+
+    expect(presenter.getHabits()[0].count).toBe(0);
+  });
+
+  it("delete habit from the list and call update callback", () => {
+    presenter.delete(habits[0], update);
+    expect(presenter.getHabits().length).toBe(1);
+    expect(presenter.getHabits()[0].name).toBe("Running");
+    checkUpdateIsCalled();
+  });
+
+  it("adds habit to the list", () => {
+    presenter.add("Eating", update);
+    expect(presenter.getHabits()[2].name).toBe("Eating");
+    expect(presenter.getHabits()[2].count).toBe(0);
+    checkUpdateIsCalled();
+  });
+
+  it("resets all habit counts to 0", () => {
+    presenter.reset(update);
+    expect(presenter.getHabits()[0].count).toBe(0);
+    expect(presenter.getHabits()[1].count).toBe(0);
+    checkUpdateIsCalled();
+  });
+
+  function checkUpdateIsCalled() {
+    expect(update).toHaveBeenCalledTimes(1);
+    expect(update).toHaveBeenCalledWith(presenter.getHabits());
+  }
+});
